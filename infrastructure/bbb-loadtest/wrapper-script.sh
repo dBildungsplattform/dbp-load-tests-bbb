70831c4218ef33a30508ee8870684642e4e75ffa
EDIN FIDORIS <EDINPHI@GMAIL.COM>
2023-07-31 12:03:24 +0200

    Changes to Dockerfile and Helm Chart

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-#Needs to be fitted towards the final docker image
-git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
-cd dbp-load-tests-bbb
-node install
-node ./loadtest/tests/chat-test.js
+node bot-script/chat-test.js
---
+++ infrastructure/bot-script/chat-test.js
@@ -0,0 +1,15 @@
+const bigbluebot = require('./index.js');
+
+const actions = async page => {
+  //await bigbluebot.chat.send(page);
+  //await bigbluebot.audio.modal.listen(page);
+  await bigbluebot.audio.modal.microphone(page);
+  await bigbluebot.video.join(page);
+};
+
+
+const options = {
+  moderator: false,
+};
+
+bigbluebot.run(actions);
