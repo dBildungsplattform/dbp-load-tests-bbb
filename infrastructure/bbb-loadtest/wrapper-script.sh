1c81cfc9134f679b68bda408a6a005b72c491394
efidoris <edinphi@gmail.com>
2023-08-02 11:03:48 +0200

    Wrapper script and Dockerfile changed

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -3,4 +3,5 @@ git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd dbp-load-tests-bbb
 npm install
 git checkout $BRANCH
-node -u "node" $@
+useradd bottester
+-u "bottester" node $@
