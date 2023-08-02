bb52e144cc84266ef85134e3cde5aaa58674e6d3
efidoris <edinphi@gmail.com>
2023-08-02 11:12:45 +0200

    User switching before git clone and and before run

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+su root
+echo $USERNAME
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd dbp-load-tests-bbb
 npm install
 git checkout $BRANCH
-useradd bottester
--u "bottester" node $@
+su bot
+echo $USERNAME
+node $@
