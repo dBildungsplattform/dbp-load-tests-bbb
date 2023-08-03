3df103a8103101c45fa4c6a13237b3543abd2a3f
efidoris <edinphi@gmail.com>
2023-08-03 12:57:50 +0200

    removed git CHECKOUT for local testing

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 whoami
 getent passwd bot
-sudo su
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd dbp-load-tests-bbb
 chown -R bot:bot /dbp-load-tests-bbb
 npm install
-git checkout $BRANCH
 whoami
-su bot
 whoami
 su - bot -c "cd /dbp-load-tests-bbb && node ./loadtest/tests/chat-test.js"
