ac28255be71ebb387ce9917afe484efb7cc8bb4b
efidoris <edinphi@gmail.com>
2023-08-03 16:57:44 +0200

    changed git checkout argument

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -2,7 +2,7 @@
 whoami
 getent passwd bot
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
-git checkout origin/DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
+git checkout DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
 cd ./dbp-load-tests-bbb
 chown -R bot:bot /dbp-load-tests-bbb
 npm install
