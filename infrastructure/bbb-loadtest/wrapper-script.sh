9ac0b4876110e086f44a041fc8a878a408215bd9
efidoris <edinphi@gmail.com>
2023-08-03 16:42:29 +0200

    doing git checkout after git clone

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -2,11 +2,12 @@
 whoami
 getent passwd bot
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
-cd dbp-load-tests-bbb
+git checkout origin/DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
+cd ./dbp-load-tests-bbb
 chown -R bot:bot /dbp-load-tests-bbb
 npm install
 whoami
 whoami
-su -c "node $@" bot
+su -c node $@ bot
 whoami
 #su bot -c node $@
