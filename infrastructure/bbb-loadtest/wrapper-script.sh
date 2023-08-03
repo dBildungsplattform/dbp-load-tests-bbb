9810d6d6eb6bcd8ee1b1a2384989ea3619bfe230
efidoris <edinphi@gmail.com>
2023-08-03 17:03:15 +0200

    reordered exec commands in wrapper

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -2,8 +2,8 @@
 whoami
 getent passwd bot
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
-git checkout DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
 cd ./dbp-load-tests-bbb
+git checkout DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
 chown -R bot:bot /dbp-load-tests-bbb
 npm install
 whoami
