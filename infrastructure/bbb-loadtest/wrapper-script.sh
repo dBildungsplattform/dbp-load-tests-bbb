2c8cff452215279da39818d38719b5abdb66eb6e
efidoris <edinphi@gmail.com>
2023-08-03 12:55:17 +0200

    added user bot and gave permission to execute the comand

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-echo $USERNAME
-useradd -m -p 0000 bot
+whoami
+getent passwd bot
+sudo su
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd dbp-load-tests-bbb
+chown -R bot:bot /dbp-load-tests-bbb
 npm install
 git checkout $BRANCH
-echo $USERNAME
-node $@
+whoami
+su bot
+whoami
+su - bot -c "cd /dbp-load-tests-bbb && node ./loadtest/tests/chat-test.js"
