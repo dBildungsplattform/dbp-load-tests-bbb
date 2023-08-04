c4632dfce568051f13698d076c606ff402aaefe3
efidoris <edinphi@gmail.com>
2023-08-04 14:45:35 +0200

    added crhomium install in wrapper

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -5,6 +5,7 @@ git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd ./dbp-load-tests-bbb
 git checkout DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
 npm install
+apt-get install chromium
 chown -R bot:bot /dbp-load-tests-bbb
 #su -c "node $@" bot
 su -c "cd /dbp-load-tests-bbb && node ./loadtest/tests/chat-test.js" bot
