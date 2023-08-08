871bac069ef263a92a54713720bf2cc1af9a6a08
efidoris <edinphi@gmail.com>
2023-08-08 11:44:43 +0200

    testing npm install in Dockerfile

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -4,7 +4,7 @@ getent passwd bot
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd ./dbp-load-tests-bbb
 git checkout DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
-npm init -y && npm i
+#npm init -y && npm i
 chown -R bot:bot /dbp-load-tests-bbb
 chown -R bot:bot /home/bot
 chmod -R o+rwx node_modules/puppeteer/.local-chromium
