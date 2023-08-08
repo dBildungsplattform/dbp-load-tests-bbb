af656b28855b68e84fb44f939b70f8a9fd619c43
efidoris <edinphi@gmail.com>
2023-08-08 12:18:34 +0200

    added debug flag for

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -8,6 +8,7 @@ npm init -y && npm i
 chown -R bot:bot /dbp-load-tests-bbb
 chown -R bot:bot /home/bot
 chmod -R o+rwx node_modules/puppeteer/.local-chromium
+echo "running $@"
 su -c "node $@" bot
 #su -c "cd /dbp-load-tests-bbb && node ./loadtest/tests/chat-test.js" bot
 #node ./loadtest/tests/chat-test.js
