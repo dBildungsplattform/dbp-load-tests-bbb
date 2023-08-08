f046388d1adb6ea16c0f92ba5575fb524db8fa40
efidoris <edinphi@gmail.com>
2023-08-08 12:26:44 +0200

    minor fixes

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-whoami
-getent passwd bot
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd ./dbp-load-tests-bbb
 git checkout DBP-193-Extend-BBB-loadtest-with-Audio-and-Video
-npm init -y && npm i
+npm i
 chown -R bot:bot /dbp-load-tests-bbb
 chown -R bot:bot /home/bot
 chmod -R o+rwx node_modules/puppeteer/.local-chromium
-echo "running $@"
 su -c "node $@" bot
-#su -c "cd /dbp-load-tests-bbb && node ./loadtest/tests/chat-test.js" bot
-#node ./loadtest/tests/chat-test.js
