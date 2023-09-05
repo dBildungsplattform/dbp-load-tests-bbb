b23f9a62d0c5210b28d4e037309553e2d81141de
efidoris <edinphi@gmail.com>
2023-09-05 16:55:04 +0200

    trying to run server from lib folder

--- loadtest/tests/video-test.js
+++ loadtest/tests/video-test.js
@@ -1,11 +1,11 @@
 const bigbluebot = require('./index.js');
 require('../lib/action/metrics.js');
-require('../lib/action/metricsServer.js');
+// require('../lib/action/metricsServer.js');
 
 const actions = async page => {
   await bigbluebot.audio.modal.microphone(page);
   await bigbluebot.video.join(page);
-  // await metricsActions.metrics(page);
+  await metricsActions.metrics(page);
 
 };
 
