01b8eaeedeaf97c42c7198836381362e93f4c61e
efidoris <edinphi@gmail.com>
2023-09-07 14:53:56 +0200

    syntax fix

--- loadtest/lib/run.js
+++ loadtest/lib/run.js
@@ -104,7 +104,9 @@ const run = async (actions, options = {}) => {
         const { endpoint } = conf.config.browser;
         if (endpoint) {
           await browser.disconnect();
+          metricsServer.serverShutdown();
         } else {
+          metricsServer.serverShutdown();
           await browser.close();
         }
       }).catch(error => {
@@ -114,8 +116,6 @@ const run = async (actions, options = {}) => {
     }).catch(error => {
       logger.error(error);
       return error;
-    }).finally(() => {
-      metricsServer.serverShutdown();
     }));
 
     // Sync the bots entrance
