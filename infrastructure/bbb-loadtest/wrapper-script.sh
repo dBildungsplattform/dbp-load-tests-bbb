2ed0c94d45cccdf597b3c902b587790abcab0192
efidoris <edinphi@gmail.com>
2023-09-11 11:16:25 +0200

    added env variable for branch

--- infrastructure/bbb-loadtest/wrapper-script.sh
+++ infrastructure/bbb-loadtest/wrapper-script.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 git clone https://github.com/dBildungsplattform/dbp-load-tests-bbb.git
 cd ./dbp-load-tests-bbb
-git checkout DBP-195-Extend-BBB-loadtest-with-Audio-and-Video
-npm i
+git checkout $BRANCH
 su -c "node $@" bot
