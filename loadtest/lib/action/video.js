6394e22e6142bd2b59438338f8b94d925d27490b
efidoris <edinphi@gmail.com>
2023-09-15 16:38:04 +0200

    testing debug flags

--- loadtest/lib/action/video.js
+++ loadtest/lib/action/video.js
@@ -18,7 +18,7 @@ const action = {
         await util.click(page, util.buildElement(datatest.share, label.share), true);
         await util.click(page, util.buildElement(datatest.settings.start, label.settings.start), true);
       },
-      test: async page => await util.visible(page, util.buildElement(datatest.fullscreen, label.fullscreen), true),
+      // test: async page => await util.visible(page, util.buildElement(datatest.fullscreen, label.fullscreen), true),
     };
   },
   get leave() {
