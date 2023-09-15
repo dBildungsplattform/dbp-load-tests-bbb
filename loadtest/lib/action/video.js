1fe58eeb5c6b1f123c2cff3ee800943b29a6bb48
efidoris <edinphi@gmail.com>
2023-09-15 13:47:28 +0200

    added some waits between change

--- loadtest/lib/action/video.js
+++ loadtest/lib/action/video.js
@@ -16,11 +16,11 @@ const action = {
       execute: async page => {
         await util.click(page, util.buildElement(datatestAudio.modal.microphone, labelAudio.modal.microphone), true);
         await util.click(page, util.buildElement(datatestAudio.modal.audioselect, labelAudio.modal.audioselect), true);
+        await page.waitForTimeout(1000);
         await util.click(page, util.buildElement(datatest.share, label.share), true);
         await util.click(page, util.buildElement(datatest.settings.start, label.settings.start), true);
       },
       test: async page => {
-        await util.visible(page, util.buildElement(datatest.change, label.change), true);
         await util.visible(page, util.buildElement(datatest.fullscreen, label.fullscreen), true);
       },
     };
