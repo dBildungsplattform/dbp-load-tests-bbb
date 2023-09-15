b5243d2af169dac602364fad8f9d56feda006902
efidoris <edinphi@gmail.com>
2023-09-15 16:19:56 +0200

    added wait for selector

--- loadtest/lib/action/video.js
+++ loadtest/lib/action/video.js
@@ -16,7 +16,7 @@ const action = {
         await util.click(page, util.buildElement(datatestAudio.modal.microphone, labelAudio.modal.microphone), true);
         await util.click(page, util.buildElement(datatestAudio.modal.audioselect, labelAudio.modal.audioselect), true);
         await util.click(page, util.buildElement(datatest.share, label.share), true);
-        await page.waitForSelector('div[data-test="startSharingWebcam"]');
+        await page.waitForSelector('[data-test="videoPreview"]');
         await util.click(page, util.buildElement(datatest.settings.start, label.settings.start), true);
       },
       test: async page => await util.visible(page, util.buildElement(datatest.fullscreen, label.fullscreen), true),
