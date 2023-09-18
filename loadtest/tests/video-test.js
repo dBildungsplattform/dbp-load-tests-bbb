658bb2ae0621b12a86269cb9f28859c8833a5c03
efidoris <edinphi@gmail.com>
2023-09-18 10:02:11 +0200

    removed a comment

--- loadtest/tests/video-test.js
+++ loadtest/tests/video-test.js
@@ -3,7 +3,6 @@ const metricsActions = require('../lib/action/metrics.js');
 
 const actions = async page => {
   
-  // await bigbluebot.audio.modal.microphone(page);
   await bigbluebot.video.join(page);
   await metricsActions.metrics(page);
 };
